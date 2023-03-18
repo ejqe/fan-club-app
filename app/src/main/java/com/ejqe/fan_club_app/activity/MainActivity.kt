0d43c58a339c13d4d9ec868d972627883283091c
ejqe <erickeugenio218@gmail.com>
2023-03-18 22:28:55 +0800

    material bottom nav

--- app/src/main/java/com/ejqe/fan_club_app/activity/MainActivity.kt
+++ app/src/main/java/com/ejqe/fan_club_app/activity/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.ejqe.fan_club_app.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.ejqe.fan_club_app.R
+import com.ejqe.fan_club_app.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
     }
+
 }
