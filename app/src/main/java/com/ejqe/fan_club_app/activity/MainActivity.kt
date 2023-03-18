347983a349f318a6eef553321cdfe8bd259c8155
ejqe <erickeugenio218@gmail.com>
2023-03-19 00:23:33 +0800

    chip bubble bottom nav

--- app/src/main/java/com/ejqe/fan_club_app/activity/MainActivity.kt
+++ app/src/main/java/com/ejqe/fan_club_app/activity/MainActivity.kt
@@ -2,25 +2,41 @@ package com.ejqe.fan_club_app.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
 import com.ejqe.fan_club_app.R
 import com.ejqe.fan_club_app.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.ejqe.fan_club_app.fragment.HomeFragment
+import com.ejqe.fan_club_app.fragment.MemberListFragment
+import com.ejqe.fan_club_app.fragment.MusicFragment
+import com.ejqe.fan_club_app.fragment.ProfileFragment
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController
 
-        binding.bottomNav.setupWithNavController(navController)
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it) {
+                R.id.memberListFragment -> loadFragment(MemberListFragment())
+                R.id.musicFragment -> loadFragment(MusicFragment())
+                R.id.profileFragment -> loadFragment(ProfileFragment())
+                else -> loadFragment(HomeFragment())
+            }
+        }
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+
 
 }
